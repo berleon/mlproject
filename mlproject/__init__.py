8cd9ac34651fe290afc0427195e5f02a7071dc11
berleon <github@leon-sixt.de>
2018-09-15 08:07:37 +0000

    Clean up

--- mlproject/__init__.py
+++ mlproject/__init__.py
@@ -1,16 +1,1 @@
-from sacred.run import Run
-import sacred
-
-def make_ml_project(ex: sacred.Experiment, data_loader, model_loader):
-    return MLProject(ex, data_loader, model_loader)
-
-def ml_main(_run: sacred.run.Run):
-    sacred.commands.print_config(_run)
-
-
-    print(_run)
-    print(_run.config)
-
-
-def ml_load_experiment():
-    pass
+from mlproject.mlproject import MLProject
